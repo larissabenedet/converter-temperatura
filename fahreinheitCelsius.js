475e30449c15aa9b894e727ad73f0b98080fc9e0
larissabenedet <larissavb2014@outlook.com>
2021-02-19 20:32:29 -0300

    resultado em fh arredondado

---
+++ fahreinheitCelsius.js
@@ -0,0 +1,15 @@
+function converte() {
+    let tempF = document.getElementById("tempF").value;
+
+    if (tempF == "") {
+        alert("Insira a temperatura em Fahreinheit")
+        exit
+    } 
+
+    let temperatura = (tempF - 32) * 5 / 9; 
+    temperatura = temperatura.toFixed(2); /* arredondando o número */
+
+    document.getElementById("tempC").value = temperatura;
+    
+
+}
